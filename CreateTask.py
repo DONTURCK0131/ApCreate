2e3e80d0eda7e602dffab6f37dcb7f04e46b136b
DONTURCK0131 <dturck23@groton.org>
2023-03-01 21:13:21 -0500

    updates

--- CreateTask.py
+++ CreateTask.py
@@ -2,14 +2,25 @@ from graphicsforcreate import *
 
 def main():
     window = GraphWin("Guess the Flag", 400, 600, autoflush = False)
-    window.set(0, 0, 800, 800)
+    window.setCoords(0, 0, 800, 800)
 
 # Work here lucas
 
 # Work here Donovan
+def flagAppearance(UserInput:str) -> (str, float):
+    inputBox = Entry(Point(200, 300), 25)  
+    UserInput = inputBox.getText()
+    
+    FlagNames = ["Kosovo", "Italy", "France"]
+
+    KosovoImage = Image(Point(200, 500), "KosovoFlag.png")
+    ItalyImage = Image(Point(200, 500), "ItalyFlag.png")
+    FranceImage = Image(Point(200, 500), "FranceFlag.png")
+
+    FlagDict = {FlagNames[0]:KosovoImage, FlagNames[1]:ItalyImage, FlagNames[2]:FranceImage}
+
 
 
 
     
 main()
-    
